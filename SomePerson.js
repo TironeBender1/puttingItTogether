5091819892c80519badd9a33c7d81dc6024c12f4
TironeBender1 <108382896+TironeBender1@users.noreply.github.com>
2022-09-11 22:01:55 -0700

    Add files via upload

---
+++ SomePerson.js
@@ -0,0 +1,18 @@
+import React, { useState } from 'react';
+
+
+const SomePerson = (props) => {
+    const { firstName, lastName, age, hairColor } = props;
+    const [stateAge, setStateAge] = useState(age);
+        return (
+    <div>
+        <h2 style={{color:"red"}}>{lastName}, {firstName}</h2>
+        <p>Age: {stateAge}</p>
+        <p>Hair Color: {hairColor}</p>
+        <button onClick={() => setStateAge(stateAge + 1)}>Birthday Button for {firstName} {lastName}</button>
+    </div>
+ ) 
+ 
+}
+
+export default SomePerson;
